8a96f2796f24cd204e00f05cf72f3502782f4b75
François Monniot <francoismonniot@gmail.com>
2021-09-14 08:24:19 -0700

    Start coding the actual plugin (maybe)
    + fix the build :)

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,8 +1,15 @@
-import {wait} from '../src/wait'
-import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
 import {expect, test} from '@jest/globals'
+
+// Example function to show how to use jest. Will be replaced when we have real stuff to test.
+async function wait(milliseconds: number): Promise<string> {
+  return new Promise(resolve => {
+    if (isNaN(milliseconds)) {
+      throw new Error('milliseconds not a number')
+    }
+
+    setTimeout(() => resolve('done!'), milliseconds)
+  })
+}
 
 test('throws invalid number', async () => {
   const input = parseInt('foo', 10)
@@ -15,15 +22,4 @@ test('wait 500 ms', async () => {
   const end = new Date()
   var delta = Math.abs(end.getTime() - start.getTime())
   expect(delta).toBeGreaterThan(450)
-})
-
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecFileSyncOptions = {
-    env: process.env
-  }
-  console.log(cp.execFileSync(np, [ip], options).toString())
 })
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,51 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+
+/** Status codes for the action. */
+enum Status {
+  SUCCESS = 'success',
+  FAILED = 'failed',
+  RUNNING = 'running'
+}
+
+// Note: core.debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 
 async function run(): Promise<void> {
+  let status: Status = Status.RUNNING
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const restricted_paths: string = core.getInput('restricted_paths')
+    const only_people: string[] = core.getMultilineInput('only_people')
+    const github_token = core.getInput('github_token')
+    core.debug(`Restricting ${restricted_paths} to ${only_people}`)
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const octokit = github.getOctokit(github_token)
+    octokit.log.debug('Make eslint happy by using octokit')
+
+    // TODO Check if restricted_paths exists in repo and warn if it doesn't ?
+    // TODO Get diff
+    // TODO Filter diff based on `restricted_paths`
+    // Compile all authors in the filtered diff
+    // if author list is contained within `only_people`
+    //    set status = succeed
+    // else
+    //    set status = failed
+
+    status = Status.SUCCESS
 
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     core.setFailed(error.message)
+    status = Status.FAILED
+  } finally {
+    core.info(
+      `${
+        status === Status.SUCCESS
+          ? 'Authorized to continue! ✅'
+          : status === Status.FAILED
+          ? 'Deployment failed! ❌'
+          : "A decision hasn't been reach, please report this bug 🐛"
+      }`
+    )
   }
 }
 
