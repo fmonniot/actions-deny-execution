267525460d4203303a0aea0ee711d5590c07a3e3
François Monniot <francoismonniot@gmail.com>
2021-09-15 08:23:37 -0700

    add some wip on git head/base detection

--- src/git.ts
+++ src/git.ts
@@ -1,3 +1,8 @@
+import {
+  CreateEvent,
+  PullRequestEvent,
+  PushEvent
+} from '@octokit/webhooks-definitions/schema'
 import {WebhookPayload} from '@actions/github/lib/interfaces'
 
 interface Refs {
@@ -24,5 +29,39 @@ export function getRefs(
   eventName: string,
   payload: WebhookPayload
 ): Refs | null {
-  return null
+  if (eventName === 'create') {
+    const createPayload = payload as CreateEvent
+    // new tag is available in GITHUB_REF, commit pointed at is GITHUB_SHA
+    // TODO Look at the ref to know if it is a tag or a branch. Skip branch.
+    if (createPayload.ref_type === 'tag') {
+      // new tag
+      const to = createPayload.ref
+      const from = `refs/heads/${createPayload.master_branch}` // TODO refs/heads/ or heads/
+
+      return {from, to}
+    } else {
+      // new branch. Does the push event is also triggered ?
+    }
+    return null
+  } else if (eventName === 'push') {
+    const pushPayload = payload as PushEvent
+    // new tag is available in GITHUB_REF, commit pointed at is GITHUB_SHA
+    // TODO Can it be triggered on a pull request ? If yes, filter out (needs to know the main branch for that)
+    const from = pushPayload.before
+    const to = pushPayload.after
+
+    return {from, to}
+  } else if (eventName === 'pull_request') {
+    const prPayload = payload as PullRequestEvent
+    // Look at event types opened, synchronize, reopened or ready_for_review
+    // GITHUB_REF points to the PR merge branch (refs/pull/:prNumber/merge)
+    // GITHUB_SHA points to the latest commit on the merge branch
+    // TODO action type closed ? Or rely on the push type ?
+    const from = prPayload.pull_request.base.ref
+    const to = prPayload.pull_request.head.ref
+
+    return {from, to}
+  } else {
+    return null
+  }
 }
